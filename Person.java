11b6c884e8352b5cc8818d75c8a7df0ec2bbe78c
FiNkacho <sabaxuxunashvili@gmail.com>
2018-06-29 14:17:08 +0400

    I Love You

---
+++ Person.java
@@ -0,0 +1,21 @@
+public class Person{
+
+	public static void main(String[] args) {
+		Human i = new Human("me");
+		Human you = new Human("The wife");
+
+		i.love(you);
+	}
+}
+
+class Human {
+	private String name;
+
+	Human(String name){
+			this.name = name;
+	}
+
+	public void love(Human other){
+		System.out.printf("%s! Will you marry me? \n", other.name);
+	}
+}
